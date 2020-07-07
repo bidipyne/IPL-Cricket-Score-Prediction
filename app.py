13cb997a6d02304f2836c6d176a5498ab9a85723
bidipyne <pyne.bidisha2017@gmail.com>
2020-07-07 18:59:26 +0530

    inial

---
+++ app.py
@@ -0,0 +1,65 @@
+from flask import Flask, jsonify, request, render_template
+import util
+import sys
+import logging
+
+app = Flask(__name__)
+
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
+
+@app.route('/')
+def home():
+    return render_template('app.html')
+
+@app.route('/venue', methods=['GET'])
+def get_venue_list():
+    venue_list = util.venue_list()
+    venue_list = [each.title() for each in venue_list]
+    response = jsonify({
+            'venue' : venue_list
+        })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route('/batting', methods=['GET'])
+def get_batting_list():
+    batting_list = util.batting_team()
+    batting_list = [each.replace('bat_team_', '').title() for each in batting_list]
+    response = jsonify({
+            'batting' : batting_list
+        })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route('/bowling', methods=['GET'])
+def get_bowling_list():
+    bowling_list = util.bowling_team()
+    bowling_list = [each.replace('bowl_team_', '').title() for each in bowling_list]
+    response = jsonify({
+            'bowling' : bowling_list
+        })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    batting = request.form['batting']
+    bowling = request.form['bowling']
+    venue =  request.form['venue']
+    overs = float(request.form['overs'])
+    runs = int(request.form['runs'])
+    wickets = int(request.form['wickets'])
+    pruns = int(request.form['pruns'])
+    pwickets = int(request.form['pwickets'])
+
+    output = util.score_estimation(runs, wickets, overs, pruns, pwickets, venue, batting, bowling)
+    return render_template('app.html', predicted_score="Predicted score {}".format(output))
+    #return response
+
+
+if __name__=='__main__':
+    util.load_artifacts()
+    print('Server started running....')
+    app.run(debug=True)
+
---
+++ util.py
@@ -0,0 +1,60 @@
+import json
+import pickle
+import numpy as np
+
+__data_columns = None
+__venue = None
+__bat_team = None
+__bowl_team = None
+__model = None
+
+def venue_list():
+    return __venue
+
+def batting_team():
+    return __bat_team
+
+def bowling_team():
+    return __bowl_team
+
+def load_artifacts():
+    print('*****Loading Artifacts Started*****')
+    global __data_columns
+    global __venue
+    global __bat_team
+    global __bowl_team
+    with open('columns.json', 'r') as f:
+        __data_columns = json.load(f)['data_columns']
+        __venue = __data_columns[5:16]
+        __bat_team = __data_columns[16:24]
+        __bowl_team = __data_columns[24:32]
+	
+    global __model
+    with open('./first-innings-score-lasso-model.pkl', 'rb') as fp:
+        __model = pickle.load(fp)
+    print('****Artifacts Loaded Successfully****')
+    
+def score_estimation(runs, wickets, overs, pruns, pwickets,venue, batting, bowling):
+    venue_index = __data_columns.index(venue.lower())
+    batting_index = __data_columns.index('bat_team_'+batting.lower())
+    bowling_index = __data_columns.index('bowl_team_'+bowling.lower())
+    
+    x  = np.zeros(len(__data_columns))
+    x[0] = runs
+    x[1] = wickets
+    x[2] = overs
+    x[3] = pruns
+    x[4] = pwickets
+    if venue_index > 0 :
+        x[venue_index] = 1
+    if batting_index > 0 :
+        x[batting_index] = 1
+    if bowling_index > 0 :
+        x[bowling_index] = 1
+        
+    return int(__model.predict([x])[0])
+    
+if __name__=='__main__':
+    load_artifacts()
+    print('Server started running....')
+    #app.run()
